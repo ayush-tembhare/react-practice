432121c9816a8a48b30f14392f30a2311bdbc031
ayush-tembhare <ayushtembhare123@gmail.com>
2026-08-28 20:12:03 +0530

    Added Props in Cards

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -9,9 +9,14 @@ const App = () => {
       <h1>Hello React Practice Day 2</h1>
      
       <Navbar/>
-       <Card/> 
-       <Card/>
-        <Card/>
+       <Card user="Ayush" age={20} city="Nagpur"/> 
+<Card user="Sammer" age={22} city="Rampur"/>
+<Card user="Ramesh" age={30} city="Mumbai"/>
+<Card user="Amit" age={25} city="Nagpur"/>
+<Card user="Rahul" age={28} city="Pune"/>
+<Card user="Priya" age={21} city="Delhi"/>
+<Card user="Sneha" age={24} city="Bangalore"/>
+<Card user="Vikas" age={27} city="Hyderabad"/>
       <Footer/>
 
     </div>
--- react-practice/src/Components/Card.jsx
+++ react-practice/src/Components/Card.jsx
@@ -1,11 +1,9 @@
 
-  
-
-const Card = () => {
-     const name = "Ayush";
+const Card = (props) => {
+     console.log(props.age);
     return(
         <div className="Card">
-            <h1>My name is {name}</h1>
+            <h1>My name is {props.user}, and age is {props.age} from {props.city}</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio suscipit qui fugiat dicta modi recusandae non pariatur aspernatur, unde sed commodi. Asperiores et libero sit voluptate quidem numquam quos id?</p>
         </div>
     )
