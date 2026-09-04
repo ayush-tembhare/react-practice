11036de8bff80cca1839ab412e2c38006322413f
ayush-tembhare <ayushtembhare123@gmail.com>
2026-09-05 00:24:33 +0530

    UI done

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,121 +1,70 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='bg-black h-screen'>
 
-      <div className="ticks"></div>
+      <h1 className='text-center bg-[#1A1A1D] text-white text-3xl p-5 font-bold'>
+        Notes App
+      </h1>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <div className='flex'>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <div className='w-[50%]'>
+
+          <h2 className='text-white text-center p-5 bg-[#3B1C32] mt-2 ml-5 text-2xl rounded-2xl'>
+            Create Note
+          </h2>
+
+          <form>
+
+            <input
+              className='text-white ml-20 mt-5 rounded-2xl pl-5 w-[80%] h-30 m-5 bg-[#6A1E55] text-3xl'
+              type='text'
+              placeholder='Enter Notes Title'
+            />
+
+            <input
+              className='text-white ml-20 mt-5 rounded-2xl pl-5 w-[80%] h-30 m-5 bg-[#6A1E55] text-3xl'
+              type='text'
+              placeholder='Enter Details'
+            />
+
+            <br />
+
+            <button className='text-white ml-100 mt-5 bg-pink-950 rounded-2xl p-5'>
+              Submit
+            </button>
+
+          </form>
+
+        </div>
+
+        <div className='ml-5 border-l-2 text-white w-[50%]'>
+
+          <h1 className='text-center p-5 bg-[#3B1C32] mt-2 ml-5 text-2xl rounded-2xl'>
+            NOTES LIST
+          </h1>
+
+          <div className='h-162.5 m-5 ml-20 flex flex-wrap overflow-auto'>
+
+            <div className='m-5 w-50 h-50 bg-[#A64D79] rounded-2xl'>
+              <h1 className='text-center pt-2 font-bold'>
+                Heading
+              </h1>
+
+              <h2 className='m-4'>
+                Details Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, saepe!
+              </h2>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
