785bcb021f223b51f7fc3da0721d53a8d10c05e9
ayush-tembhare <ayushtembhare123@gmail.com>
2026-09-05 23:32:29 +0530

    created local storgae fol

---
+++ local-storage/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const App = () => {
+// ==================== LocalStorage Commands ====================
+localStorage.clear()
+
+// // Store data
+// localStorage.setItem("user", "Sarthak");
+
+// // Get data
+// localStorage.getItem("user");
+
+// // Remove one item
+// localStorage.removeItem("user");
+
+// // Clear all LocalStorage
+// localStorage.clear();
+
+
+// // ==================== SessionStorage ====================
+
+// // Store data
+// sessionStorage.setItem("user", "Sarthak");
+
+// // Get data
+// sessionStorage.getItem("user");
+
+// // Remove one item
+// sessionStorage.removeItem("user");
+
+// // Clear all SessionStorage
+// sessionStorage.clear();
+
+
+// // ==================== Object / Array ====================
+
+// // Store Object/Array → String
+// localStorage.setItem("user", JSON.stringify(user));
+
+// // Retrieve String → Object/Array
+// const user = JSON.parse(localStorage.getItem("user"));
+  
+  
+  return (
+    <div>
+      APP
+    </div>
+  )
+}
+
+export default App
